469c3e1e08887667c82eab5d4a4bbeb56304771a
Phaberen <magnusphaberen@gmail.com>
2025-09-10 11:30:22 +0200

    TouristAttraction -> Swapped City & Description attributes (in constructor and its parameter as well)
    TouristRepository - > attributes now match the hardcoded data and shows properly in localhost test.
    
    TouristController -> added the model parameter, attribute name + value. Now compatible with thymeleaf & HTML
    
    Added a test HTML and CSS file for localhost test

--- src/main/java/tourism/controller/TouristController.java
+++ src/main/java/tourism/controller/TouristController.java
@@ -1,24 +1,27 @@
 package tourism.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tourism.repository.TouristRepository;
+import tourism.service.TouristService;
 
 @Controller
 @RequestMapping("attractions")
 public class TouristController {
 
-//    private final TouristService service;
+    private final TouristService service;
 
-//    public TouristController(TouristService service) {
-//        this.service = service;
-//    }
-
+    public TouristController(TouristService service) {
+        this.service = service;
+    }
     //    GET /attractions
     @GetMapping
-    public String getAttractions(){
-        return "attractionsList";
+    public String getAttractions(Model model){
+        model.addAttribute("attractions", service.getAttractions());
+        return "attractionList";
     }
     //    GET /attractions/{name}
     @GetMapping("{name}")
